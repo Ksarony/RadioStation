128f462c96aab883115a6144e05ab9338cd2db02
saronykashfia <saronykashfia@gmail.com>
2023-01-12 01:47:38 +0100

    Set up pipenv.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn binarystartle.wsgi
+web: gunicorn radiostation.wsgi
--- radiostation/wsgi.py
+++ radiostation/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for binarystartle project.
+WSGI config for radiostation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'binarystartle.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'radiostation.settings')
 
 application = get_wsgi_application()
